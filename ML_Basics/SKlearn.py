77a16f7c1c303f4b9a04e121aaf60a174a261f25
Chayanika-Basak <cb250802@gmail.com>
2022-05-15 12:51:14 +0530

    ML Basics

---
+++ ML_Basics/SKlearn.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+
+iris_dataset = datasets.load_iris()
+
+X = iris_dataset.data[:,:2]
+
+x_count = len(X.flat)
+x_min = X[:,0].min() - 0.5
+x_max = X[:,0].max() + 0.5
+x_mean = X[:,0].mean()
+
+print((x_count,x_min, x_max,x_mean,))
