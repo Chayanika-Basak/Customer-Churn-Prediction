d7d1c9287d792d8ee1be3ba7d43de47db5c4ad80
Chayanika-Basak <cb250802@gmail.com>
2022-05-16 12:15:05 +0530

    LinearRegression

--- ML_Basics/LinearRegression.py
+++ ML_Basics/LinearRegression.py
@@ -4,10 +4,10 @@ from matplotlib import *
 from matplotlib.pyplot import *
 from sklearn.linear_model import *
 
-data = {'area' : [2600,3000,3200,3600,4000],
+train_data = {'area' : [2600,3000,3200,3600,4000],
 'price': [550000,565000,610000,680000,725000]}
 
-df = DataFrame(data, columns=['area','price'])
+df = DataFrame(train_data, columns=['area','price'])
 
 print(df)
 # xlabel('area(sq ft)')
@@ -21,3 +21,16 @@ print(reg.fit(df[['area']],df.price))
 
 # price = coef*(area) + intercept (Just like y = m*x + c)
 print(reg.predict([[3300]]), reg.coef_, reg.intercept_)
+
+test_data = {'area' : [1000,1500,2300,3540,4120,4560,5490,3460,4750,2300,9000,8600,7100]}
+
+df2 = DataFrame(test_data, columns=['area'])
+
+res = reg.predict(df2)
+ires=[]
+for val in res:
+    val = int(val)
+    ires.append(val)
+df2['prices'] = ires
+print(df2)
+df2.to_csv('prediction.csv', index=False)
