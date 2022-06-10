9afb3ae2bb2a580d3cfef0c72dc4d2c888f2b6ae
Chayanika Basak <cb250802@gmail.com>
2022-06-10 12:39:19 +0530

    SVD started

---
+++ ML_Maths/Vectors.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+'''Vector_3_4.png'''
+# plt.quiver(0,0,3,4,scale_units='xy',angles='xy',scale=1)
+# plt.xlim(0,6)
+# plt.ylim(0,6)
+# plt.show()
+
+'''MultipleVectors.png'''
+# plt.quiver(0,0,3,4,scale_units='xy',angles='xy',scale=1, color='r')
+# plt.quiver(0,0,3,-7,scale_units='xy',angles='xy',scale=1, color='b')
+# plt.xlim(-10,6)
+# plt.ylim(-10,6)
+# plt.show()
+
